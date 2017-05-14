12e4899b6cb1414dac2231211052b48bb5a033f0
Reji.Mathews <reji.mathews@target.com>
2017-05-14 13:28:04 +0530

    baseline learners material

---
+++ src/main/java/com/poc/accountservice/AccountServiceApplication.java
@@ -0,0 +1,14 @@
+package com.poc.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by z001nlh on 5/14/17.
+ */
+@SpringBootApplication
+public class AccountServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
+}

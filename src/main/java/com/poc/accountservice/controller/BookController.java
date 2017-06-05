462d74cf6091730789f8961a0f29134881480c3f
Reji Mathews <reji@Rejis-MacBook-Pro.local>
2017-06-05 09:30:53 +0530

    renamed read method

--- src/main/java/com/poc/accountservice/controller/BookController.java
+++ src/main/java/com/poc/accountservice/controller/BookController.java
@@ -4,7 +4,6 @@ import com.poc.accountservice.entity.book.Book;
 import com.poc.accountservice.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
@@ -26,13 +25,15 @@ public class BookController {
         return new ResponseEntity<List<Book>>(bookService.retrieveAllBooks(),HttpStatus.OK);
     }
 
+
+    //CRUD SECTION STARTS
     @RequestMapping(value = "/books", method = RequestMethod.PUT)
     public ResponseEntity createABook(@RequestBody Book book){
         return new ResponseEntity<Book>(bookService.saveBook(book),HttpStatus.OK);
     }
 
     @RequestMapping(value = "/books/{bookId}", method = RequestMethod.GET)
-    public ResponseEntity createABook(@PathVariable("bookId") Integer bookId){
+    public ResponseEntity retrieveABook(@PathVariable("bookId") Integer bookId){
         return new ResponseEntity<Book>(bookService.getBookDetails(bookId),HttpStatus.OK);
     }
 
@@ -57,5 +58,6 @@ public class BookController {
         return new ResponseEntity<String>("Book details deleted",HttpStatus.OK);
     }
 
+    //CRUD SECTION ENDS
 
 }
